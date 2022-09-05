c80b1d254826c8bb9d8577c7543bc10ea464d362
Leejieon <42794501+Leejieon@users.noreply.github.com>
2022-09-05 11:19:09 +0900

    [Silver III] Title: 돌 게임 3, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static boolean[] dp = new boolean[1001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp[1] = true;
+        dp[2] = false;
+        dp[3] = true;
+        dp[4] = true;
+        for(int i=5;i<=N;++i){
+            dp[i]=false;
+            if(!dp[i-1]||!dp[i-3]||!dp[i-4])
+                dp[i] = true;
+        }
+        System.out.println(dp[N] ? "SK" : "CY");
+    }
+
+}
