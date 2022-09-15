14255a9412f82c3ffd72a8817f6ae6dd615ee7e2
Leejieon <42794501+Leejieon@users.noreply.github.com>
2022-09-15 15:14:23 +0900

    [Silver I] Title: 동물원, Time: 212 ms, Memory: 18484 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        long[] dp = new long[N + 1];
+        dp[0] = 1;
+        dp[1] = 3;
+        for (int i = 2; i <= N; ++i){
+            dp[i] = (dp[i-1]*2 + dp[i-2])%9901;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
