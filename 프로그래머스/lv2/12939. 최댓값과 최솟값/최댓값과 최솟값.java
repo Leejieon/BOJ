b1e1f268e348ba6287e9945ef89f65ae98b309b7
leejieon <42794501+Leejieon@users.noreply.github.com>
2023-08-13 13:55:49 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.52 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        List<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(s, " ");
+        
+        while(st.hasMoreTokens()){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        Collections.sort(list);
+        
+        answer = list.get(0) + " " + list.get(list.size() - 1);
+        
+        return answer;
+    }
+}
