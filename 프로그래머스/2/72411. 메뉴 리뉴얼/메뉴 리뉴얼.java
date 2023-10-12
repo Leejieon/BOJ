ebb295b12f3dfab6176416103fa84f198dac2028
leejieon <42794501+Leejieon@users.noreply.github.com>
2023-10-12 17:41:28 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 27.94 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    
+    static List<String> result = new ArrayList<>();
+    static Map<String, Integer> map = new HashMap<>();
+    
+    public List<String> solution(String[] orders, int[] course) {
+        String[] answer = {};
+        
+        for (int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = String.valueOf(arr);
+        }
+        
+        for(int courseLen : course) {
+            for(String order : orders){
+                combination("", order, courseLen);
+            }
+            findMaxValue();
+            map.clear();
+            
+        }
+        
+        Collections.sort(result);
+        
+        return result;
+    }
+    
+    static void combination(String order, String others, int count) {
+        if (count == 0) {
+            map.put(order, map.getOrDefault(order, 0) + 1);
+            return;
+        }
+
+        // 0 ~ length까지 조합
+        for (int i = 0; i < others.length(); i++)
+            combination(order + others.charAt(i), others.substring(i + 1), count - 1);
+    }
+    
+    // 가장 많은 조합 찾기
+    static void findMaxValue() {
+        if(map.isEmpty())
+            return;
+        
+        List<Integer> list = new ArrayList<>(map.values());
+        int max = Collections.max(list);
+        
+        if(max > 1){
+            for(String key : map.keySet()) {
+                if(map.get(key) == max)
+                    result.add(key);
+            }
+        }
+    }
+}
