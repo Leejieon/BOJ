2ee61cd4d27cfd1e255e39a949d2ad011b7fdedc
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-01-24 23:19:28 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16764 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] numbers;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+
+        permutation(0, 0);
+        System.out.print(sb);
+    }
+
+    static void permutation(int depth, int prev) {
+        // Base Case
+        if (depth == M) {
+            for (int number : numbers) {
+                sb.append(number).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        // Recursive Case
+        for (int cand = prev; cand < N; cand++) {
+            numbers[depth] = cand + 1;
+            permutation(depth + 1, cand);
+        }
+    }
+}
