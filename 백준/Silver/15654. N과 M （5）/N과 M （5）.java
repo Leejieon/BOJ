0feb6f43ff9dd92f3fad441be4259677cc8ab6ea
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-01-24 23:31:33 +0900

    [Silver III] Title: N과 M (5), Time: 352 ms, Memory: 29932 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -12,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
@@ -27,7 +26,8 @@ public class Main {
 
         Arrays.sort(numbers);
         permutation(0);
-        System.out.print(sb);
+        bw.write(sb.toString());
+        bw.close();
     }
 
     static void permutation(int depth) {
