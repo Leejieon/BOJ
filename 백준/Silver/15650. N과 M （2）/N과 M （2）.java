787de5521f310c70435f5f7f160ac84055a332d0
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-01-24 23:09:51 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] numbers;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+        visited = new boolean[N];
+
+        permutation(0);
+        System.out.print(sb);
+    }
+
+    static void permutation(int depth) {
+        // Base Case
+        if (depth == M) {
+            if (checkNumber()) {
+                for (int number : numbers) {
+                    sb.append(number).append(" ");
+                }
+                sb.append("\n");
+            }
+            return;
+        }
+
+        // Recursive Case
+        for (int cand = 0; cand < N; cand++) {
+            if (!visited[cand]) {
+                visited[cand] = true;
+                numbers[depth] = cand + 1;
+                permutation(depth + 1);
+                visited[cand] = false;
+            }
+        }
+    }
+
+    static boolean checkNumber() {
+        int prev = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (prev > numbers[i]) {
+                return false;
+            }
+            prev = numbers[i];
+        }
+        return true;
+    }
+}
