9bc9a72d302824c5a403944ae8e91113f5619798
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-01-25 13:46:46 +0900

    [Silver II] Title: N과 M (12), Time: 140 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] numbers, order;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        order = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        permutation(0, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void permutation(int depth, int prev) {
+        // Base Case
+        if (depth == M) {
+            for (int number : order) {
+                sb.append(number).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        // Recursive Case
+        int prevNum = 0;
+        for (int cand = prev; cand < numbers.length; cand++) {
+            if (prevNum != numbers[cand]) {
+                order[depth] = numbers[cand];
+                prevNum = numbers[cand];
+                permutation(depth + 1, cand);
+            }
+
+        }
+    }
+}
