c6ea39b6b01d41ef6bc6a21b0f40a7b2e54b83a8
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-02-28 09:20:01 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 244 ms, Memory: 19300 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,92 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
+// 문제 꼼꼼히 읽기.
+public class Main {
+    static class data implements Comparable<data>{
+        int x;
+        int y;
+        int w;
+        public data(int x, int y, int w) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+        }
+        @Override
+        public int compareTo(data o) {
+            return w-o.w;
+        }
 
-	static class point implements Comparable<point> {
+        @Override
+        public String toString() {
+            return "data{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", w=" + w +
+                    '}';
+        }
+    }
+    static int[][] delta = new int[][] {{0,1},{1,0},{-1,0},{0,-1}};
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int row, col, cost;
+        int t = 1;
+        while(true){
+            n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
 
-		public point(int row, int col, int cost) {
-			super();
-			this.row = row;
-			this.col = col;
-			this.cost = cost;
-		}
+            int[][] board = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-		@Override
-		public int compareTo(point o) {
-			return this.cost - o.cost;
-		}
 
-	}
+            PriorityQueue<data> pq = new PriorityQueue<>();
+            int[][] make = new int[n][n];
+            for (int[] ints : make) {
+                Arrays.fill(ints, 999999999);
+            }
+            pq.add(new data(0,0,board[0][0]));
+            make[0][0] = board[0][0];
 
-	static int N;
-	static int[][] map;
-	static int[][] distance; 
-	static int[] dy = { 0, 1, -1, 0 }; 
-	static int[] dx = { 1, 0, 0, -1 };
+            while (!pq.isEmpty()){
+                data poll = pq.poll();
 
-	static boolean isValid(int x, int y) {
-		if (x < 0 || x >= N || y < 0 || y >= N)
-			return false;
-		return true;
-	}
+                for (int d = 0; d < 4; d++) {
+                    int dy = poll.y + delta[d][0];
+                    int dx = poll.x + delta[d][1];
+                    if (check_size(dy, dx)) {
+                        if (make[dy][dx] > make[poll.y][poll.x] + board[dy][dx]) {
+                            make[dy][dx] = make[poll.y][poll.x] + board[dy][dx];
+                            pq.add(new data(dx, dy, make[dy][dx]));
+                        }
+                    }
+                }
+            }
 
-	public static int dijkstra() {
-		PriorityQueue<point> pq = new PriorityQueue<point>();
-		distance[0][0] = map[0][0];
-		pq.offer(new point(0, 0, map[0][0]));
+            System.out.printf("Problem %d: %d\n",t++,make[n-1][n-1]);
+        }
+    }
 
-		while (!pq.isEmpty()) {
-			point p = pq.poll();
-
-			for (int k = 0; k < 4; k++) {
-				int nextRow = p.row + dy[k];
-				int nextCol = p.col + dx[k];
-
-				if (isValid(nextRow, nextCol)) {
-					if (distance[nextRow][nextCol] > distance[p.row][p.col] + map[nextRow][nextCol]) { 
-						distance[nextRow][nextCol] = distance[p.row][p.col] + map[nextRow][nextCol]; 
-						pq.offer(new point(nextRow, nextCol, distance[nextRow][nextCol])); 
-					}
-				}
-			}
-		}
-		return distance[N - 1][N - 1];
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int cnt = 0;
-        
-		while (true){
-			N = Integer.parseInt(br.readLine());
-            
-			if (N == 0) 
-				break; 
-            
-			map = new int[N][N];
-			distance = new int[N][N];
-
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-					distance[i][j] = Integer.MAX_VALUE;
-				}
-			} 
-			cnt++;
-			sb.append("Problem " + cnt + ": " + dijkstra() + "\n");
-		}
-        
-		System.out.println(sb); 
-        br.close();
-	}
-
+    static boolean check_size(int y , int x){
+        return 0 <= y && y < n && 0 <= x && x < n;
+    }
 }
+
