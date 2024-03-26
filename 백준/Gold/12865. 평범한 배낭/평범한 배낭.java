d699b4b22400151bac5d39aecbfad6b0dd4ac290
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-03-26 13:20:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 200 ms, Memory: 53844 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,49 +1,42 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[N+1][K+1];
+		int[] W = new int[N+1];
+		int[] V = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=K; j++) {
+				if(j < W[i]) {
+					dp[i][j] = dp[i-1][j];
+				}
+			
+				else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
 
-        ArrayList<Item> items = new ArrayList<>();
-        items.add(new Item(0, 0));
-        int[][] dp = new int[N + 1][K + 1];
-        for (int i = 1; i <= N; i++) {
-            items.add(new Item(sc.nextInt(), sc.nextInt()));
-        }
-        Collections.sort(items);
-
-
-        for (int item = 1; item <=N ; item++) {
-            for (int weight = 1; weight <= K; weight++) {
-                if (weight < items.get(item).weight) {
-                    dp[item][weight] = dp[item - 1][weight];
-                }
-                else{
-                    dp[item][weight] = Math.max(dp[item-1][weight], dp[item-1][weight-items.get(item).weight] + items.get(item).value);
-                }
-            }
-        }
-
-        System.out.println(dp[N][K]);
-    }
-
-    static class Item implements Comparable<Item>{
-        int weight;
-        int value;
-
-        public Item(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(Item o) {
-            // 무게를 기준으로 오름차순 정렬
-            return this.weight - o.weight;
-        }
-    }
+				}
+			}
+		}
+		System.out.println(dp[N][K]);
+	}
 }
